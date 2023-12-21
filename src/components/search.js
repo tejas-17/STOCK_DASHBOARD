ca12afebf304c2f1ff624fc53f3b042b07a5993c
Tejas Nimbre <tejas.nimbre@npci.org.in>
2023-12-21 17:24:06 +0530

    added search bar for stocks

---
+++ src/components/search.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Autocomplete from '@mui/material/Autocomplete';
+
+export default function FreeSolo() {
+  return (
+   
+      <Autocomplete
+        freeSolo
+        id="free-solo-2-demo"
+        disableClearable
+        options={top100Films.map((option) => option.title)}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Search input"
+            InputProps={{
+              ...params.InputProps,
+              type: 'search',
+            }}
+          />
+        )}
+      />
+  
+  );
+}
+
+const top100Films = [
+    { label: 'aapl' },
+    { label: 'goog'},
+    { label: 'msft'},
+    { label: 'tsla'},
+];
+
